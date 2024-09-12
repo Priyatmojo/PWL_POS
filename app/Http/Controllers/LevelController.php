ee89e6c294fbcae5cb0e1482f1b960c8c1ba4f0b
Mangsen <priyatmojoew@gmail.com>
2024-09-12 22:55:37 +0700

    Praktikum 6 - Jobsheet 3

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,7 +9,6 @@ class LevelController extends Controller
 {
     public function index() {
         // DB::insert('insert into m_level(level_kode, level_nama, created_at) value(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
-
         // return 'Insert data baru berhasil';
 
         // $row = DB::update('update m_level set level_nama= ? where level_kode = ?', ['Customer', 'CUS']);
